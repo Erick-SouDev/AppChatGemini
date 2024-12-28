329cdd55cd2cfebf1f240a7d02566c0370e4fda7
Erick-SouDev <erick1991java@gmail.com>
2024-12-28 16:54:42 -0300

    teste

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
+import { HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { MessageboxComponent } from "./components/messagebox/messagebox.component";
 
 @Component({
   selector: 'app-root',
-  imports: [ MessageboxComponent],
+  imports: [ MessageboxComponent ,HttpClientModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/components/messagebox/messagebox.component.ts
+++ src/app/components/messagebox/messagebox.component.ts
@@ -1,13 +1,11 @@
-import { Component } from '@angular/core';
-import { TextareaModule } from 'primeng/textarea';
 import { FormsModule } from '@angular/forms';
-import { InputTextModule } from 'primeng/inputtext';
-import { ButtonModule } from 'primeng/button';
+import { Component } from '@angular/core';
 import { Observable } from 'rxjs';
+
 
 @Component({
   selector: 'app-messagebox',
-  imports: [TextareaModule , FormsModule  , InputTextModule  ,ButtonModule],
+  imports: [ FormsModule],
   templateUrl: './messagebox.component.html',
   styleUrl: './messagebox.component.css'
 })
@@ -17,7 +15,5 @@ export class MessageboxComponent {
   response?:Observable<string> ;
   valor?:string = "ola";
 
-  enviar():void{
-    alert("ola teste button");
-  }
+
 }
